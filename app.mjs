824a5e318da30537e0b8577cf62f93046ec2bb97
Ayesha ghaffar <gayesha759@gmail.com>
2025-02-19 10:44:33 +0500

    statingBackend

---
+++ app.mjs
@@ -0,0 +1,25 @@
+import express from "express"
+const app = express()
+const port = process.env.PORT || 8080
+import path from "path"
+
+const __dirname = path.resolve()
+app.use("/" , express.static(path.join(__dirname,"./frontend/dist")))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/weather', (req, res) => {
+    res.send('weather is sunny in karachi!')
+  })
+  app.get('/weather/:cityName', (req, res) => {
+    res.send(req.params)
+  })
+  app.get('/weather/:cityName/:side', (req, res) => {
+    res.send(req.params)
+  })
+  
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-center text-blue-500">
+        Hello, it's me Ayesha! It's my 1st frontend connected by my backend.
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
